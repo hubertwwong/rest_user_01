f54bb64c7dde894e664b4cf875916bef70cead93
Hubert Wong <hubertwwong@gmail.com>
2014-02-27 00:52:14 -0800

    index json works...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,41 @@
 class UsersController < ApplicationController
+
+  respond_to :json
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+    respond_with(@users)
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def update
+    @user = User.find(params[:id])
+  end
+  
+  private
+
+  # strong params..
+  # new to rails 4.
+  # basically you have to explictly permit params.
+  # probably don't want the user_id to be allowed.
+  # devise should be keeping track of this for you
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
+
 end
